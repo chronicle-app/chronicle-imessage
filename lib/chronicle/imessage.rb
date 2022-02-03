b054cf202948ccc3d0c8965340fadbc89f1ce067
Andrew Louis <andrew@hyfen.net>
2022-02-03 00:02:53 -0500

    New gem

---
+++ lib/chronicle/imessage.rb
@@ -0,0 +1,8 @@
+require "chronicle/imessage/version"
+
+module Chronicle
+  module Imessage
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/chronicle/imessage/version.rb
@@ -0,0 +1,5 @@
+module Chronicle
+  module Imessage
+    VERSION = "0.1.0"
+  end
+end
